e88bc8428565d81e0b9a3a382c8961828bc52176
kate0912 <eunha912@naver.com>
2016-09-10 21:59:52 +0900

    chatroom_emoticon2

--- js/sendTextInput.js
+++ js/sendTextInput.js
@@ -17,6 +17,7 @@ $(document).ready(function () {
 
     if (login_check === 1) { // -------------------------------------------------------- 로그인일 경우
         // phone-body 높이 맞춰주기
+        body_height = $('.phone-wrapper').height() - 46;
         $('.phone-body').css('height', body_height + 'px').css('min-height', body_height + 'px');
 
         // 이모티콘 아이콘, 텍스트 입력, 전송 버튼
@@ -46,80 +47,92 @@ $(document).ready(function () {
 
         // 이모티콘 아이콘 눌렀을 경우
         $('#button_plus').on("click", function () {
-            emoticon_mode = 1; // 이모티콘 모드
 
-            // phone-body 높이 변경
-            $('.phone-body').css('height', '60%').css('min-height', '60%');
-            $('.phone-body').scrollTop($('.phone-body')[0].scrollHeight); // 스크롤 맨 아래
+            if (emoticon_mode === 0) {
+                emoticon_mode = 1; // 이모티콘 모드
 
-            // emoticon_area 나타내주기
-            $('#input_area').append(
-                $('<div>').attr('id', 'emoticon_area').append(
-                    $('<div>').attr('class', 'carousel carousel-slider center').attr('data-indicators', 'true').append(
-                        $('<div>').attr('class', 'carousel-item').attr('href', '#one!').append(
-                            $('<div>').attr('id', 'emoticon_collection').append(
-                                $('<div>').attr('class', 'row emoticon_row').append(
-                                    $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png'),
-                                    $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test1.png'),
-                                    $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test2.png'),
-                                    $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png')
-                                ),
-                                $('<div>').attr('class', 'row emoticon_row').append(
-                                    $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png'),
-                                    $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png'),
-                                    $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png'),
-                                    $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png')
+                // phone-body 높이 변경
+                $('.phone-body').css('height', '60%').css('min-height', '60%');
+                $('.phone-body').scrollTop($('.phone-body')[0].scrollHeight); // 스크롤 맨 아래
+
+                // emoticon_area 나타내주기
+                $('#input_area').append(
+                    $('<div>').attr('id', 'emoticon_area').append(
+                        $('<div>').attr('class', 'carousel carousel-slider center').attr('data-indicators', 'true').append(
+                            $('<div>').attr('class', 'carousel-item').attr('href', '#one!').append(
+                                $('<div>').attr('id', 'emoticon_collection').append(
+                                    $('<div>').attr('class', 'row emoticon_row').append(
+                                        $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png'),
+                                        $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test1.png'),
+                                        $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test2.png'),
+                                        $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png')
+                                    ),
+                                    $('<div>').attr('class', 'row emoticon_row').append(
+                                        $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png'),
+                                        $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png'),
+                                        $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png'),
+                                        $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png')
+                                    )
                                 )
-                            )
-                        ),
-                        $('<div>').attr('class', 'carousel-item').attr('href', '#two!').append(
-                            $('<h2>').text('Second Panel')
+                            ),
+                            $('<div>').attr('class', 'carousel-item').attr('href', '#two!').append(
+                                $('<h2>').text('Second Panel')
 
-                        ),
-                        $('<div>').attr('class', 'carousel-item').attr('href', '#three!').append(
-                            $('<h2>').text('Third Panel')
+                            ),
+                            $('<div>').attr('class', 'carousel-item').attr('href', '#three!').append(
+                                $('<h2>').text('Third Panel')
+                            )
                         )
                     )
                 )
-            )
 
-            // 이모티콘 누르면 선택한 이모티콘 입력창 위쪽에 나타내기
-            $('.emoticon').on("click", function () {
-                emoticon_src = $(this).attr('src');
+                // 이모티콘 누르면 선택한 이모티콘 입력창 위쪽에 나타내기
+                $('.emoticon').on("click", function () {
+                    emoticon_src = $(this).attr('src');
 
-                if (select_emoticon === 0) { // 처음 선택한 경우
-                    $('.carousel.carousel-slider').before(
-                        $('<div>').attr('id', 'select_emoticon_area').append(
-                            $('<img>').attr('id', 'select_emoticon').attr('class', 'emoticon').attr('src', emoticon_src),
-                            $('<img>').attr('id', 'cancel').attr('src', './images/x.png').attr('alt', 'cancel')
+                    if (select_emoticon === 0) { // 처음 선택한 경우
+                        $('.carousel.carousel-slider').before(
+                            $('<div>').attr('id', 'select_emoticon_area').append(
+                                $('<img>').attr('id', 'select_emoticon').attr('class', 'emoticon').attr('src', emoticon_src),
+                                $('<img>').attr('id', 'cancel').attr('src', './images/x.png').attr('alt', 'cancel')
+                            )
                         )
-                    )
 
-                    select_emoticon = 1;
-                } else if (select_emoticon === 1) { // 연속해서 다시 선택한 경우
-                    $('#select_emoticon').attr('src', emoticon_src);
-                    select_emoticon = 1;
-                }
+                        select_emoticon = 1;
+                    } else if (select_emoticon === 1) { // 연속해서 다시 선택한 경우
+                        $('#select_emoticon').attr('src', emoticon_src);
+                        select_emoticon = 1;
+                    }
 
-                // select_emoticon_area 위치 조정
-                var select_area = $('#select_emoticon_area').height() + 40;
-                $('#select_emoticon_area').css('top', '-' + select_area + 'px');
+                    // select_emoticon_area 위치 조정
+                    var select_area = $('#select_emoticon_area').height() + 40;
+                    $('#select_emoticon_area').css('top', '-' + select_area + 'px');
 
-                // 이모티콘 입력창 위쪽 x누르면 없어지게 하기
-                $('#cancel').on("click", function () {
-                    $('#select_emoticon_area').remove();
-                    select_emoticon = 0;
+                    // 이모티콘 입력창 위쪽 x누르면 없어지게 하기
+                    $('#cancel').on("click", function () {
+                        $('#select_emoticon_area').remove();
+                        select_emoticon = 0;
+                    });
+
                 });
 
-            });
+                // 초기화
+                $('.carousel.carousel-slider').carousel({ full_width: true });
 
-            // 초기화
-            $('.carousel.carousel-slider').carousel({ full_width: true });
+            }
+        });
 
+        // 엔터 버튼에 대한 이벤트 등록
+        $('#m').keydown(function (key) {
+
+            if (key.keyCode == 13) {
+                alert('enter');
+            }
 
         });
 
-        // 전송 버튼에 이벤트 등록
+
+        // 전송 버튼에 대한 이벤트 등록
         $('#button_submit').on("click", function () {
             var message = $('#m').val();
             var nowTime = getNowTime();
@@ -146,6 +159,8 @@ $(document).ready(function () {
                     // 이모티콘 영역 삭제
                     $('#input_area').children().last().remove();
                     emoticon_mode = 0; // 이모티콘 모드 변수 초기화
+
+                    body_height = $('.phone-wrapper').height() - 46;
                     $('.phone-body').css('height', body_height + 'px').css('min-height', body_height + 'px'); // phone-body 높이 맞춰주기
 
                     // 채팅창에 메시지 붙이기
@@ -174,7 +189,10 @@ $(document).ready(function () {
                     // 이모티콘 영역 삭제
                     $('#input_area').children().last().remove();
                     emoticon_mode = 0; // 이모티콘 모드 변수 초기화
+
+                    body_height = $('.phone-wrapper').height() - 46;
                     $('.phone-body').css('height', body_height + 'px').css('min-height', body_height + 'px'); // phone-body 높이 맞춰주기
+                    $('.phone-body').scrollTop($('.phone-body')[0].scrollHeight); //스크롤 맨 아래로
 
                     select_emoticon = 0; //이모티콘 선택 여부 초기화
 
@@ -187,6 +205,10 @@ $(document).ready(function () {
         });
 
     } else if (login_check === 0) { // --------------------------------------------------------로그인이 아닐 경우
+        // phone-body 높이 맞춰주기
+        body_height = $('.phone-wrapper').height() - 46;
+        $('.phone-body').css('height', body_height + 'px').css('min-height', body_height + 'px');
+
         // 로그인 연결 아이콘으로 만들기, 텍스트 입력 불가 및 placeholder
         input.append(
             $('<div>').attr('id', 'button_plus').attr('class', 'btn waves-effect waves-light').append(
