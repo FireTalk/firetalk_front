f7055c4022f27055a337e116b2df834255628b02
kate0912 <eunha912@naver.com>
2016-09-14 20:18:10 +0900

    chatroom event

--- js/getMessage.js
+++ js/getMessage.js
@@ -1,27 +1,4 @@
 $(document).ready(function () {
-    /*
-        // 현재 시간을 얻어주는 함수
-        function getNowTime() {
-            var now = new Date();
-            var nowTime;
-    
-            if(now.getHours() > 12) {
-                nowTime = "오후 " + (now.getHours()-12) + ":" + now.getMinutes();
-            } else if(now.getHours() === 12) {
-                nowTime = "오후 " + now.getHours() + ":" + now.getMinutes();
-            } else{
-                nowTime = "오전 " + now.getHours() + ":" + now.getMinutes();
-            }
-    
-            alert(nowTime);
-        }
-    */
-    /*
-    window.onload = function () {
-        getNowTime();
-    }
-    */
-
     // 스크롤 하단으로 이동시키기
     window.onload = function () {
         $('.phone-body').scrollTop($('.phone-body')[0].scrollHeight);
@@ -32,27 +9,25 @@ $(document).ready(function () {
 
     // 메시지 리스트 배열 (MAX>>몇개까지 정해야겠지..)
     var message_list = [
-        { profile: 'profile1.png', nickname: '공블리', msg: '완전 웃긴닼ㅋㅋㅋㅋ 상황정황 얼척없음 ㅋㅋㅋㅋㅋ', people: '24', time: '오후 6:16' },
-        { profile: 'profile1.png', nickname: '공블리', msg: 'ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ', people: '24', time: '오후 6:16' },
-        { profile: '', nickname: '', msg: '솔찍히 공블리 때문에 보는 것도 있었는데 연기 역시 잘해~', people: '1', time: '오후 6:16' },
-        { profile: 'profile1.png', nickname: '공블리', msg: '제가 봤을 때 이거 작가가 약하고 쓴 것이 틀림없음', people: '24', time: '오후 6:16' }
+        { profile: 'profile1.png', nickname: '공블리', msg: '완전 웃긴닼ㅋㅋㅋㅋ 상황정황 얼척없음 ㅋㅋㅋㅋㅋ'},
+        { profile: 'profile1.png', nickname: '공블리', msg: 'ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ'},
+        { profile: '', nickname: '', msg: '솔찍히 공블리 때문에 보는 것도 있었는데 연기 역시 잘해~'},
+        { profile: 'profile1.png', nickname: '공블리', msg: '제가 봤을 때 이거 작가가 약하고 쓴 것이 틀림없음'}
     ];
 
     // 배열>>메시지 리스트 보여주기
     $.each(message_list, function (index, item) {
         $(function () {
-            $('.collection').addMsgList(item.profile, item.nickname, item.msg, item.people, item.time);
+            $('.collection').addMsgList(item.profile, item.nickname, item.msg);
         });
     })
 
     // 메시지 리스트 아이템 add해주는 함수
-    $.fn.addMsgList = function (profile, nickname, msg, people, time) {
+    $.fn.addMsgList = function (profile, nickname, msg) {
         if (profile === '' && nickname === '') { //내가 보낸 메시지
             this.append(
                 $('<div>').attr('class', 'row').append(
                     $('<li>').attr('class', 'collection-item avatar my-msg').append(
-                        $('<span>').attr('class', 'my-people').text(people),
-                        $('<span>').attr('class', 'my-time').text(time),
                         $('<p>').attr('class', 'msg2').text(msg)
                     )
                 )
@@ -63,9 +38,7 @@ $(document).ready(function () {
                     $('<li>').attr('class', 'collection-item avatar').append(
                         $('<img>').attr('src', 'images/' + profile).attr('class', 'circle'),
                         $('<span>').attr('class', 'nickname').text(nickname),
-                        $('<p>').attr('class', 'msg1').text(msg),
-                        $('<span>').attr('class', 'secondary-content badge people').text(people),
-                        $('<span>').attr('class', 'secondary-content badge time').text(time)
+                        $('<p>').attr('class', 'msg1').text(msg)
                     )
                 )
             );
@@ -73,7 +46,7 @@ $(document).ready(function () {
     }
 
     // 같은 사람이 여러개 보낸 경우 메시지 리스트 아이템 add해주는 함수
-    $.fn.addMultiMsgList = function (profile, nickname, msg, people, time) {
+    $.fn.addMultiMsgList = function (profile, nickname, msg) {
 
     }
 
--- js/sendTextInput.js
+++ js/sendTextInput.js
@@ -14,7 +14,6 @@ $(document).ready(function () {
 
 
 
-
     if (login_check === 1) { // -------------------------------------------------------- 로그인일 경우
         // phone-body 높이 맞춰주기
         body_height = $('.phone-wrapper').height() - 46;
@@ -30,6 +29,9 @@ $(document).ready(function () {
             ),
             $('<button>').attr('id', 'button_submit').attr('class', 'btn waves-effect waves-light').attr('type', 'button').text('전송')
         )
+
+        // 최근 온 메시지 위치 이동
+        scrollBottom('어이가 아리마생 이 상황 뭥미? ㅋㅋ 진짜 어마무시한 전개다 예측불허 ㅋㅋㅋㅋ 너무 웃곀ㅋ');
 
         // 텍스트 입력시 전송 버튼 글씨 opacity 100%로 변경
         $("#m").keyup(function () {
@@ -58,35 +60,14 @@ $(document).ready(function () {
                 $('.phone-body').scrollTop($('.phone-body')[0].scrollHeight); // 스크롤 맨 아래
 
                 // emoticon_area 나타내주기
-                $('#input_area').append(
-                    $('<div>').attr('id', 'emoticon_area').append(
-                        $('<div>').attr('class', 'carousel carousel-slider center').attr('data-indicators', 'true').append(
-                            $('<div>').attr('class', 'carousel-item').attr('href', '#one!').append(
-                                $('<div>').attr('id', 'emoticon_collection').append(
-                                    $('<div>').attr('class', 'row emoticon_row').append(
-                                        $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png'),
-                                        $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test1.png'),
-                                        $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test2.png'),
-                                        $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png')
-                                    ),
-                                    $('<div>').attr('class', 'row emoticon_row').append(
-                                        $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png'),
-                                        $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png'),
-                                        $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png'),
-                                        $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png')
-                                    )
-                                )
-                            ),
-                            $('<div>').attr('class', 'carousel-item').attr('href', '#two!').append(
-                                $('<h2>').text('Second Panel')
+                showEmoticionArea();
 
-                            ),
-                            $('<div>').attr('class', 'carousel-item').attr('href', '#three!').append(
-                                $('<h2>').text('Third Panel')
-                            )
-                        )
-                    )
-                )
+                // 이모티콘 더블 클릭하면 보내지기
+                $('.emoticon').dblclick(function () {
+                    emoticon_src = $(this).attr('src');
+                    select_emoticon = 1;
+                    sendMsgEvent();
+                });
 
                 // 이모티콘 누르면 선택한 이모티콘 입력창 위쪽에 나타내기
                 $('.emoticon').on("click", function () {
@@ -126,84 +107,15 @@ $(document).ready(function () {
 
         // 엔터 버튼에 대한 이벤트 등록
         $('#m').keydown(function (key) {
-
             if (key.keyCode == 13) {
-                alert('enter');
+                sendMsgEvent();
+                return false;
             }
-
         });
-
 
         // 전송 버튼에 대한 이벤트 등록
         $('#button_submit').on("click", function () {
-            var message = $('#m').val();
-            var nowTime = getNowTime();
-
-            if (emoticon_mode === 0) { // 이모티콘 모드가 아닐 경우
-
-                if (text_input === 1) { // 텍스트만 보낼 경우
-                    // 채팅창에 메시지 붙이기
-                    $('.collection').addMsgList('', '', message, 20, nowTime);
-
-                    // 메시지 입력창 초기화
-                    $('#m').val('');
-                    text_input = 0;
-                    // 스크롤 맨 아래로
-                    $('.phone-body').scrollTop($('.phone-body')[0].scrollHeight);
-                }
-            } else if (emoticon_mode === 1) { // 이모티콘 모드일 경우
-
-                if (text_input === 0 && select_emoticon === 0) { // 아무것도 보내지 않음
-                    console.log("입력을 안함");
-
-                } else if (text_input === 1 && select_emoticon === 0) { // 텍스트만 보내기
-
-                    // 이모티콘 영역 삭제
-                    $('#input_area').children().last().remove();
-                    emoticon_mode = 0; // 이모티콘 모드 변수 초기화
-
-                    body_height = $('.phone-wrapper').height() - 46;
-                    $('.phone-body').css('height', body_height + 'px').css('min-height', body_height + 'px'); // phone-body 높이 맞춰주기
-
-                    // 채팅창에 메시지 붙이기
-                    $('.collection').addMsgList('', '', message, 20, nowTime);
-
-
-                    $('#m').val(''); // 메시지 입력창 초기화
-                    text_input = 0; // 텍스트 입력 여부 초기화
-
-                    $('.phone-body').scrollTop($('.phone-body')[0].scrollHeight); //스크롤 맨 아래로
-
-
-                } else if (text_input === 0 && select_emoticon === 1) { // 이모티콘만 보내기
-
-                    // 채팅방에 이모티콘 붙이기
-                    $('.collection').append(
-                        $('<div>').attr('class', 'row').append(
-                            $('<li>').attr('class', 'collection-item avatar my-msg').append(
-                                $('<span>').attr('class', 'send_emoticon_people').text(20),
-                                $('<span>').attr('class', 'send_emoticon_time').text(nowTime),
-                                $('<img>').attr('class', 'send_emoticon').attr('src', emoticon_src)
-                            )
-                        )
-                    );
-
-                    // 이모티콘 영역 삭제
-                    $('#input_area').children().last().remove();
-                    emoticon_mode = 0; // 이모티콘 모드 변수 초기화
-
-                    body_height = $('.phone-wrapper').height() - 46;
-                    $('.phone-body').css('height', body_height + 'px').css('min-height', body_height + 'px'); // phone-body 높이 맞춰주기
-                    $('.phone-body').scrollTop($('.phone-body')[0].scrollHeight); //스크롤 맨 아래로
-
-                    select_emoticon = 0; //이모티콘 선택 여부 초기화
-
-                } else if (text_input === 1 && select_emoticon === 1) { // 텍스트랑 이모티콘 함께 보내기
-
-
-                }
-            }
-
+            sendMsgEvent();
         });
 
     } else if (login_check === 0) { // --------------------------------------------------------로그인이 아닐 경우
@@ -229,5 +141,146 @@ $(document).ready(function () {
 
     }
 
+
+    // ---------------------------------------------함수들-------------------------------------------//
+    // 최근 온 메시지 맨 아래로 내려가게 하는 함수 
+    function scrollBottom(msg) {
+        // 최근온 메시지 바로 내려가기 기능
+        $('#button_plus').before(
+            $('<div>').attr('id', 'bottom_message').append(
+                $('<img>').attr('id', 'message_profile').attr('class', 'circle').attr('src', 'images/profile1.png'),
+                $('<div>').attr('id', 'bottom_text').text(msg),
+                $('<img>').attr('id', 'scroll_bottom').attr('src', 'images/bottom_scroll.png').attr('alt', 'scroll_bottom')
+            )
+        )
+        // 최근 온 메시지 위치로 맨 아래로 스크롤 내리기
+        $('#scroll_bottom').on("click", function () {
+            $('.phone-body').scrollTop($('.phone-body')[0].scrollHeight); // 스크롤 맨 아래
+        });
+    }
+
+    // 전송에 대한 기능 함수 
+    function sendMsgEvent() {
+        var message = $('#m').val();
+        //var nowTime = getNowTime();
+
+        if (emoticon_mode === 0) { // 이모티콘 모드가 아닐 경우
+
+            if (text_input === 1) { // 텍스트만 보낼 경우
+                // 채팅창에 메시지 붙이기
+                $('.collection').addMsgList('', '', message);
+
+                // 메시지 입력창 초기화
+                $('#m').val('');
+                text_input = 0;
+                // 스크롤 맨 아래로
+                $('.phone-body').scrollTop($('.phone-body')[0].scrollHeight);
+            }
+        } else if (emoticon_mode === 1) { // 이모티콘 모드일 경우
+            if (text_input === 0 && select_emoticon === 0) { // 아무것도 보내지 않음
+                console.log("입력을 안함");
+
+            } else if (text_input === 1 && select_emoticon === 0) { // 텍스트만 보내기
+
+                // 이모티콘 영역 삭제
+                $('#input_area').children().last().remove();
+                emoticon_mode = 0; // 이모티콘 모드 변수 초기화
+
+                body_height = $('.phone-wrapper').height() - 46;
+                $('.phone-body').css('height', body_height + 'px').css('min-height', body_height + 'px'); // phone-body 높이 맞춰주기
+
+                // 채팅창에 메시지 붙이기
+                $('.collection').addMsgList('', '', message);
+
+
+                $('#m').val(''); // 메시지 입력창 초기화
+                text_input = 0; // 텍스트 입력 여부 초기화
+
+                $('.phone-body').scrollTop($('.phone-body')[0].scrollHeight); //스크롤 맨 아래로
+
+
+            } else if (text_input === 0 && select_emoticon === 1) { // 이모티콘만 보내기
+
+                // 채팅방에 이모티콘 붙이기
+                $('.collection').append(
+                    $('<div>').attr('class', 'row').append(
+                        $('<li>').attr('class', 'collection-item avatar my-msg').append(
+                            $('<img>').attr('class', 'send_emoticon').attr('src', emoticon_src)
+                        )
+                    )
+                );
+
+                // 이모티콘 영역 삭제
+                $('#input_area').children().last().remove();
+                emoticon_mode = 0; // 이모티콘 모드 변수 초기화
+
+                body_height = $('.phone-wrapper').height() - 46;
+                $('.phone-body').css('height', body_height + 'px').css('min-height', body_height + 'px'); // phone-body 높이 맞춰주기
+                $('.phone-body').scrollTop($('.phone-body')[0].scrollHeight); //스크롤 맨 아래로
+
+                select_emoticon = 0; //이모티콘 선택 여부 초기화
+
+            } else if (text_input === 1 && select_emoticon === 1) { // 텍스트랑 이모티콘 함께 보내기
+                // 채팅방에 텍스트  이모티콘 붙이기
+                $('.collection').append(
+                    $('<div>').attr('class', 'row').append(
+                        $('<li>').attr('class', 'collection-item avatar my-msg2').append(
+                            $('<img>').attr('class', 'send_emoticon2').attr('src', emoticon_src)
+                        ),
+                        $('<li>').attr('class', 'collection-item avatar my-msg2').append(
+                            $('<p>').attr('class', 'msg2').text(message)
+                        )
+                    )
+                );
+
+                // 이모티콘 영역 삭제
+                $('#input_area').children().last().remove();
+                emoticon_mode = 0; // 이모티콘 모드 변수 초기화
+
+                // 메시지 입력창 초기화
+                $('#m').val('');
+                text_input = 0;
+
+                body_height = $('.phone-wrapper').height() - 46;
+                $('.phone-body').css('height', body_height + 'px').css('min-height', body_height + 'px'); // phone-body 높이 맞춰주기
+                $('.phone-body').scrollTop($('.phone-body')[0].scrollHeight); //스크롤 맨 아래로
+
+                select_emoticon = 0; //이모티콘 선택 여부 초기화
+
+            }
+        }
+    }
+
+    function showEmoticionArea() {
+        $('#input_area').append(
+            $('<div>').attr('id', 'emoticon_area').append(
+                $('<div>').attr('class', 'carousel carousel-slider center').attr('data-indicators', 'true').append(
+                    $('<div>').attr('class', 'carousel-item').attr('href', '#one!').append(
+                        $('<div>').attr('id', 'emoticon_collection').append(
+                            $('<div>').attr('class', 'row emoticon_row').append(
+                                $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png'),
+                                $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test1.png'),
+                                $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test2.png'),
+                                $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png')
+                            ),
+                            $('<div>').attr('class', 'row emoticon_row').append(
+                                $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png'),
+                                $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png'),
+                                $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png'),
+                                $('<img>').attr('class', 'col s3 emoticon').attr('src', 'images/emoticon/emoticon_test.png')
+                            )
+                        )
+                    ),
+                    $('<div>').attr('class', 'carousel-item').attr('href', '#two!').append(
+                        $('<h2>').text('Second Panel')
+
+                    ),
+                    $('<div>').attr('class', 'carousel-item').attr('href', '#three!').append(
+                        $('<h2>').text('Third Panel')
+                    )
+                )
+            )
+        )
+    }
 
 });
