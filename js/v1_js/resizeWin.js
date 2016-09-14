bcb9b9d51ab1e0be7be55bc1a3df7aeff643ea83
kate0912 <eunha912@naver.com>
2016-09-14 20:23:23 +0900

    delete v1 files

--- js/v1_js/resizeWin.js
+++
@@ -1,5 +0,0 @@
-// 윈도우 창 크기 변경 막기
-window.addEventListener('resize', function () {
-    console.log('Resized');
-    window.resizeTo(375, 647);
-});
--- js/v1_js/slideNav.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    // slidNav 사용하기 위한 기본
-    $('.button-collapse').sideNav();
-    
-})
