61d48b2952fbc614d4dd654a025c32fc8f379e30
LeeJuEun <wndms0116@naver.com>
2016-09-18 04:25:57 +0900

    message

--- js/duplicationCheck.js
+++ js/duplicationCheck.js
@@ -1,7 +1,21 @@
 function duplicationCheck() {
     var nick = document.getElementById("input_nick").value;
     var duplication = document.getElementById("duplication");
+    /*
+    var exist = ["nick1", "nick2", "nick3"];
 
+    for (var i = 0; i < exist.length; i++)
+    {
+        if (nick == exist[i]) {
+            duplication.style.color = "green";
+            duplication.style.display = "block";
+        }
+        else {
+            duplication.style.display = "none";
+        }
+    }
+    */ //비교하는거 모르겠...
+    
     if (nick.length >= 1) {
         duplication.style.color = "green";
         duplication.style.display = "block";
